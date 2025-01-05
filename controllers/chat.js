ad29791c010213530c670df0073123db1eae6d95
chiragdhunna <chiragdhunna2468@gmail.com>
2025-01-05 19:51:44 +0530

    updating alert event

--- controllers/chat.js
+++ controllers/chat.js
@@ -178,12 +178,10 @@ const removeMember = TryCatch(async (req, res, next) => {
 
   await chat.save();
 
-  emitEvent(
-    req,
-    ALERT,
-    chat.members,
-    `${userThatWillBeRemoved.name} has been removed from the group`
-  );
+  emitEvent(req, ALERT, chat.members, {
+    message: `${userThatWillBeRemoved.name} has been removed from the group`,
+    chatId,
+  });
 
   emitEvent(req, REFETCH_CHATS, allChatMembers);
 
@@ -226,7 +224,10 @@ const leaveGroup = TryCatch(async (req, res, next) => {
     chat.save(),
   ]);
 
-  emitEvent(req, ALERT, chat.members, `${user} left the group`);
+  emitEvent(req, ALERT, chat.members, {
+    message: `${user} left the group`,
+    chatId,
+  });
 
   return res.status(200).json({
     success: true,
