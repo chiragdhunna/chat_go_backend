58cefb4ad56f50a694da3bea986782d773c22a0b
chiragdhunna <chiragdhunna2468@gmail.com>
2025-01-05 17:16:17 +0530

    leave group message update

--- controllers/chat.js
+++ controllers/chat.js
@@ -230,7 +230,7 @@ const leaveGroup = TryCatch(async (req, res, next) => {
 
   return res.status(200).json({
     success: true,
-    message: "Member removed successfully",
+    message: "Leave Group Successfully",
   });
 });
 
